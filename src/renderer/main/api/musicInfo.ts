97e5c604705d9ab39d1f7b7450faffac62c06ac0
Joey <786678257@qq.com>
2023-03-29 18:30:37 +0800

    fix: music url

--- src/renderer/main/api/musicInfo.ts
+++ src/renderer/main/api/musicInfo.ts
@@ -15,15 +15,15 @@ export function fetchMusicUrl2(mid: string | number): Promise<string> {
   }).then((res) => res.data.data.url)
 }
 // https://antiserver.kuwo.cn/anti.s?type=convert_url&format=mp3&response=url&rid=228491562
-export function fetchMusicUrl(rid: string | number): Promise<string> {
+// const target_url = `http://www.kuwo.cn/api/v1/www/music/playUrl?mid=${song_id}&type=convert_url3&br=128kmp3`;
+export function fetchMusicUrl(mid: string | number): Promise<string> {
   return request({
-    url: 'https://antiserver.kuwo.cn/anti.s',
+    url: 'http://www.kuwo.cn/api/v1/www/music/playUrl',
     method: 'GET',
     params: {
-      rid,
-      type: 'convert_url',
-      format: 'mp3',
-      response: 'url',
+      mid,
+      type: 'convert_url3',
+      br: 'br=128kmp3'
     },
   }).then((res) => res.data)
 }
