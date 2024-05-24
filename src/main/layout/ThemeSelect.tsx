e22ba207e603cb5ea76be69c4d1b8cc27c4e58f8
Joey <786678257@qq.com>
2024-05-24 10:51:53 +0800

    fix: set local theme

--- src/main/layout/ThemeSelect.tsx
+++ src/main/layout/ThemeSelect.tsx
@@ -22,6 +22,7 @@ function getTheme(): Theme {
 }
 
 function setLocalTheme(theme: Theme) {
+  localStorage.setItem(LOCAL_THEME, theme)
   window.electronAPI.setTheme(theme).then(() => {
     const root = window.document.documentElement
     root.classList.remove('light', 'dark')
