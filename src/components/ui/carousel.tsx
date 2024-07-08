34b3c78b291cbab88f56bea9303411e3d408beab
Joey <786678257@qq.com>
2024-07-08 16:36:24 +0800

    fix: 停止轮播图在鼠标进入时自动播放

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -53,7 +53,7 @@ const Carousel = React.forwardRef<
         ...opts,
         axis: orientation === 'horizontal' ? 'x' : 'y',
       },
-      [Autoplay({ playOnInit: true })]
+      [Autoplay({ playOnInit: true, stopOnMouseEnter: true })]
     )
     const [canScrollPrev, setCanScrollPrev] = React.useState(false)
     const [canScrollNext, setCanScrollNext] = React.useState(false)
