15d8569b19e37eead4f1ef891965dfeab9dc423d
Joey <786678257@qq.com>
2024-07-19 15:36:13 +0800

    docs(electron-builder): 更新发布说明和许可协议

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -60,7 +60,6 @@ module.exports = {
   releaseInfo: {
     releaseName: `v${version}`,
     releaseNotes: '1. 修复已知问题\n2. 优化用户体验\n',
-    releaseNotesFile: 'resources/release-notes.md',
     releaseDate: new Date().toLocaleString('zh-CN'),
   },
   // publish: {
