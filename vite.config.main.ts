a47bd82dd1c558d1193bf1f0b9aee3a424678e5f
Joey <786678257@qq.com>
2025-07-23 14:31:34 +0800

    feat: electronPackagePlugin

--- vite.config.main.ts
+++ vite.config.main.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { builtinModules } from "node:module";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
+import pkg from "./package.json";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -13,19 +14,34 @@ const EXTERNAL = builtinModules
   .concat(builtinModules)
   .concat("electron", "electron/main", "electron/common", "electron/renderer");
 
+function electronPackagePlugin(): Plugin {
+  let outDir = "dist";
+  return {
+    name: "electron-package",
+    configResolved(config) {
+      outDir = config.build.outDir;
+    },
+    buildStart(options) {
+      return options.fs.writeFile(
+        path.join(outDir, "package.json"),
+        JSON.stringify({
+          name: pkg.name,
+          version: pkg.version,
+          main: "main.mjs",
+          dependencies: pkg.dependencies,
+        })
+      );
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [electronPackagePlugin()],
   build: {
     target: `node${NODE_VERSION}`,
     outDir: path.join(ROOT, "dist"),
     emptyOutDir: false,
-    // lib: {
-    //   entry: {
-    //     main: path.join(ROOT, '/src-main/index.ts'),
-    //   },
-    //   formats: ['es'],
-    //   fileName: (_format, entryName) => entryName + '.mjs',
-    // },
     rollupOptions: {
       input: {
         main: path.join(ROOT, "/src-main/index.ts"),
