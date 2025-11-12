8d2342200fb5a8561e024f72e78fcfca5f53b17b
Joey <786678257@qq.com>
2025-11-12 09:10:26 +0800

    Update error.tsx

--- src/main/layout/error.tsx
+++ src/main/layout/error.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link, useRouteError } from "react-router";
 import icon from "../assets/icon.png";
-import ThemeToggle from "./theme-toggle";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Error() {
   const error = useRouteError() as { statusText: string; message: string };
@@ -15,12 +15,12 @@ export default function Error() {
           </Link>
           <div className="flex-1 h-full draggable"></div>
           {import.meta.env.DEV && <button onClick={() => window.devAPI.toggleDevtools()}>devtools</button>}
-          <ThemeToggle />
+          <ModeToggle />
         </div>
       </header>
       <div className="max-w-lg mx-auto text-center py-20 px-6 flex gap-2 flex-col">
         <h1 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0">出错了!</h1>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">发生错误了,请稍后再试或向开发者反馈!</p>
+        <p className="leading-7 not-first:mt-6">发生错误了,请稍后再试或向开发者反馈!</p>
         {error && (
           <p className="mt-6 italic">
             <i>{error.statusText || error.message}</i>
